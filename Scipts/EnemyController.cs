e87822299e37fcc45cb431590d42326d20c9fd9d
LlenaresLebi <madafakasbingladen@gmail.com>
2020-03-23 16:35:39 +0800

    Scripts of the game. Levi Llenares

---
+++ Scipts/EnemyController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 3f;
+    static Animator anim;
+    Transform target;
+    NavMeshAgent agent;
+
+    CharacterCombat combat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        combat = GetComponent<CharacterCombat>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            anim.SetBool("isRunning", true);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //Attack the target
+                CharacterStats targetStats = target.GetComponent<CharacterStats>();
+                if (targetStats != null)
+                {
+                    combat.Attack(targetStats);
+
+                }
+                
+                anim.SetBool("isAttacking", true);
+                FaceTarget();
+                
+               
+            }
+            else
+            {
+                anim.SetBool("isAttacking", false);
+            }
+            
+        }
+        
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotaion = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotaion, Time.deltaTime * 5f);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Scipts/Movement.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Interactable focus;
+    public LayerMask movementMask;
+    static Animator anim;
+    public float speed;
+    Camera cam;
+
+   
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationSpeed = 80f;
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        //for jumping the character
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("isJumping");
+        }
+        //for walking the character
+        if (translation != 0)
+        {
+            anim.SetBool("isWalking", true);
+            RemoveFocus();
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        //for runnig the character
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+
+            anim.SetBool("isRunning", true);
+            RemoveFocus();
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+         
+        }
+
+        //for attack animation
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            anim.SetBool("isAttacking", true);
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if(interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+    void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+
+            
+            focus = newFocus;
+        }
+       
+        newFocus.OnFocused(transform);
+        
+    }
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+ 
+    }
+}
